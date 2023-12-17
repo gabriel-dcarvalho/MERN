d18d984960c5e7f9d55962555695bf98bad30cbb
Gabriel CarvalhoCC <gabriellcarvalho.dev@gmail.com>
2023-12-17 17:02:02 -0300

    refactor: add route sum

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express')
 const app = express()
 
 
-// Metodo HTTP
-// "get" pega algo para o usuário
-// é uma função callback porque executa por tras do get
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get("/soma", (req, res) => {
+  const soma = 96 + 1;
+
+  res.send({soma: soma })
 })
 
 app.listen(3000)
